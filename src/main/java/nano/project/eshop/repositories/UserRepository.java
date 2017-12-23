41f472e8fd9dee675b017f818bc2a79c5e755b38
nanoatic <nanoaticorgix@hotmail.com>
2017-12-23 16:11:07 +0100

    basket dev

--- src/main/java/nano/project/eshop/repositories/UserRepository.java
+++ src/main/java/nano/project/eshop/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import nano.project.eshop.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository("userRepository")
@@ -11,4 +12,5 @@ public interface UserRepository extends CrudRepository<User, Long> {
      User findByEmail(String email);
      User findByConfirmationToken(String confirmationToken);
      List<User> findTop10ByOrderByIdDesc();
+     List<User> findByBdate(Date bdate);
 }
